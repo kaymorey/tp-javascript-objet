a7ffda5832caf37bce46fe221db70d2dc566074a
Katia Moreira <ktia.morei@gmail.com>
2024-02-13 21:19:23 +0100

    Add final files

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -2,16 +2,54 @@ import colors from './typesColor.js';
 
 export default class Pokemon {
   constructor(data) {
-
+    this.id = data.pokedexId;
+    this.name = data.name.fr;
+    this.img = data.sprites.regular;
+    this.types = [];
+    data.types.forEach((type) => {
+      this.types.push(type.name);
+    });
+    this.hp = data.stats.hp;
+    this.stats = {
+      attack: data.stats.atk,
+      defense: data.stats.def,
+      speed: data.stats.vit,
+    }
   }
 
   getTemplateCard() {
     const card = document.createElement('div');
     card.classList.add('card');
-    card.style.setProperty('--color', colors[/* 1er item du tableau types */]);
+    card.style.setProperty('--color', colors[this.types[0]]);
     card.innerHTML = `
-      <!-- Le code HTML de la card -->
+      <span class="card__label">HP <strong>${this.hp}</strong></span>
+      <img src="${this.img}" alt="" class="card__img">
+      <h2>${this.name}</h2>
+      <div class="card__types"></div>
+      <div class="card__stats">
+        <p>
+          attaque<br>
+          <strong class="card__number">${this.stats.attack}</strong>
+        </p>
+        <p>
+          défense<br>
+          <strong class="card__number">${this.stats.defense}</strong>
+        </p>
+        <p>
+          vitesse<br>
+          <strong class="card__number">${this.stats.speed}</strong>
+        </p>
+      </div>
     `;
+
+    const types = card.querySelector('.card__types');
+    this.types.forEach((type) => {
+      const span = document.createElement('span');
+      span.classList.add('card__type');
+      span.textContent = type;
+      span.style.setProperty('--color', colors[type]);
+      types.appendChild(span);
+    });
 
     return card;
   }
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import Pokemon from './Pokemon.js';
 class App {
   constructor() {
     this.pokemons = [];
+    this.visiblePokemons= [];
     this.types = [];
     this.container = document.querySelector('.js-cards');
     this.select = document.querySelector('.js-select');
@@ -15,9 +16,62 @@ class App {
     return json;
   }
 
+  getPokemonTypes () {
+    this.pokemons.forEach((pokemon) => {
+      pokemon.types.forEach((type) => {
+        if (!this.types.includes(type)) {
+          this.types.push(type);
+        }
+      });
+    });
+  }
+
+  fillSelect () {
+    this.types.forEach((type) => {
+      const option = document.createElement('option');
+      option.value = type;
+      option.textContent = type;
+      this.select.appendChild(option);
+    });
+  }
+
+  displayAllPokemons () {
+    this.pokemons.forEach((pokemon) => {
+      const card = pokemon.getTemplateCard();
+      this.container.appendChild(card);
+    });
+  }
+
   async main () {
     const data = await this.getPokemons();
     const pokemonsData = data.slice(0, 52);
+    this.container.innerHTML = '';
+    console.log(pokemonsData);
+    pokemonsData.forEach((pokemonData) => {
+      const pokemon = new Pokemon(pokemonData);
+      this.pokemons.push(pokemon);
+      const card = pokemon.getTemplateCard();
+      this.container.appendChild(card);
+    });
+
+    this.getPokemonTypes();
+    this.fillSelect();
+
+    this.select.addEventListener('change', () => {
+      const value = this.select.value;
+      this.container.innerHTML = '';
+      if (value) {
+        this.pokemons.forEach((pokemon) => {
+          const types = pokemon.types;
+          if (types.includes(value)) {
+            const card = pokemon.getTemplateCard();
+            this.container.appendChild(card);
+          }
+        });
+      } else {
+        this.displayAllPokemons();
+      }
+    });
   }
 }
 
